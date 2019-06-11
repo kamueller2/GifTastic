9810b7bb1c986d9d15a92f1aa8b06822f2c7dc5d
Kimberly <kamueller2@gmail.com>
2019-06-11 15:35:20 -0400

    added variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,8 @@
 let topics = ['dog', 'monkey', 'cat', 'bird'];
+
+//CREATES AN EVENT LISTENER FOR WHEN USER CLICKS ON submit button, CB THIS FUNC
+$("#button-addon2").on("click", function() {
+
+    let queryURL =
+
+})
